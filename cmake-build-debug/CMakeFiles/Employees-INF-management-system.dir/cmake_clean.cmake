ff0a462e3acca9cfd457eb29938ab6128009bc51
JustWB <justwb13@gmail.com>
2020-06-22 15:24:03 +0800

    initialization_test

---
+++ cmake-build-debug/CMakeFiles/Employees-INF-management-system.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Employees-INF-management-system.dir/main.cpp.o"
+  "Employees-INF-management-system"
+  "Employees-INF-management-system.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Employees-INF-management-system.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
